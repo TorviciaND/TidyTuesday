4cf013a74b9ae2dcf270c534daadbe3fbee9ca4d
TorviciaND <40802846+TorviciaND@users.noreply.github.com>
2022-08-23 11:52:06 -0400

    Add files via upload

--- Tidy Tuesday 82322.R
+++ Tidy Tuesday 82322.R
@@ -9,10 +9,14 @@ library(gridExtra)
 #Removes the emojis from the data set
 characteristics$personality <- iconv(characteristics$personality, "latin1", "ASCII", sub="")
 
+#####
 #Find the highest ranking "feminist" characters
 feminists <- characteristics %>% filter(personality == "feminist") %>% 
   arrange(rank)
 
+#This takes the top 25 ranked feminist characters in the data set and extracts the personality types (not feminist)
+#as long as those ratings are over a score of 90.  We then go through and create a table of the new personality 
+#values, as long as the trait shows up at least 5 times among the feminist characters.
 feminist_characteristics <- characteristics %>% 
   filter(char_name %in% feminists$char_name[1:25] & personality != "feminist" & avg_rating >= 90) %>% 
   mutate(n = 1) %>%
@@ -22,7 +26,11 @@ feminist_characteristics <- characteristics %>%
   filter(count > 4) %>%
   arrange(desc(count))
 
+#Lots of traits may have been kept, only the top 20 are included.  We start at 2 because the emoji categories
+#were left as blanks up above and this is always the most common category among possible categories
 feminist_characteristics <- feminist_characteristics[2:21,]
+
+#Adding angles to our characteristics to spice up the word clouds.
 feminist_characteristics <- feminist_characteristics %>% 
   mutate(angle = 90 * sample(c(0, 1), n(), replace = TRUE, prob = c(60, 40)))
 
@@ -39,7 +47,7 @@ p1 <- ggplot(feminist_characteristics, aes(label = personality, size = count/4, 
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         plot.title = element_text(hjust = 0.5, size = 20, color = "#ffffff", margin = margin(b = 10)))
-
+#####
 #Find the highest ranking "queer" characters
 queer <- characteristics %>% filter(personality == "queer") %>% 
   arrange(rank)
@@ -71,6 +79,7 @@ p2 <- ggplot(queer_characteristics, aes(label = personality, size = count/4, ang
         axis.ticks = element_blank(),
         plot.title = element_text(hjust = 0.5, size = 20, color = "#ffffff", margin = margin(b = 10)))
 
+#####
 #Find the highest ranking "autistic" characters
 autistic <- characteristics %>% filter(personality == "autistic") %>% 
   arrange(rank)
@@ -102,6 +111,7 @@ p3 <- ggplot(autistic_characteristics, aes(label = personality, size = count/4, 
         axis.ticks = element_blank(),
         plot.title = element_text(hjust = 0.5, size = 20, color = "#ffffff", margin = margin(b = 10)))
 
+#####
 #Find the highest ranking "nerd" characters
 nerd <- characteristics %>% filter(personality == "nerd") %>% 
   arrange(rank)
@@ -133,4 +143,6 @@ p4 <- ggplot(nerd_characteristics, aes(label = personality, size = count/4, angl
         axis.ticks = element_blank(),
         plot.title = element_text(hjust = 0.5, size = 20, color = "#ffffff", margin = margin(b = 10)))
 
+#####
+#This allows us to graph all 4 of our word clouds in one graph
 grid.arrange(p1, p2, p3, p4, nrow = 2, top = textGrob("Stereotypes in Television\nThe Most Common Characteristics of Characters that are Considered:", gp = gpar(fontsize = 24)))
